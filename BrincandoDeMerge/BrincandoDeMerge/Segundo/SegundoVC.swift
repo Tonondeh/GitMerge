fa9dafbbabeb462071b1ae556f1b4e5bac79f002
Andre <tonondeh87@gmail.com>
2021-08-25 15:01:50 -0300

    segundo arquivo com Xib, storyboard

---
+++ BrincandoDeMerge/BrincandoDeMerge/Segundo/SegundoVC.swift
@@ -0,0 +1,22 @@
+//
+//  SegundoVC.swift
+//  BrincandoDeMerge
+//
+//  Created by ANDRE LUIZ TONON on 25/08/21.
+//
+
+import UIKit
+
+class SegundoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+// testando o segundo arquivo com Xiib
+
+}
