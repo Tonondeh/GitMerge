ff7976091fc218400928423376c75d9f4fc41414
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-08-25 15:46:00 -0300

    tela 3

---
+++ BrincandoDeMerge/BrincandoDeMerge/PauloTela3/PauloTela3ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PauloTela3ViewController.swift
+//  BrincandoDeMerge
+//
+//  Created by Paulo Rodrigues on 25/08/21.
+//
+
+import UIKit
+
+class PauloTela3ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("Funciona filho da égua")
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
