3ce1577ffd431cdd49e645ebd0ba6dd940943c11
Andre <tonondeh87@gmail.com>
2021-08-25 14:42:48 -0300

    criacao do projeto

---
+++ BrincandoDeMerge/BrincandoDeMerge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrincandoDeMerge
+//
+//  Created by ANDRE LUIZ TONON on 25/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
