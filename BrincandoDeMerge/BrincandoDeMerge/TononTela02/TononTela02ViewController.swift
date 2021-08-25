9f1eb70ebc633b6d9f4b91dd294d51f380ef4d03
Andre <tonondeh87@gmail.com>
2021-08-25 15:34:55 -0300

    Criacao de Tela 02 com pastas

---
+++ BrincandoDeMerge/BrincandoDeMerge/TononTela02/TononTela02ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TononTela02ViewController.swift
+//  BrincandoDeMerge
+//
+//  Created by ANDRE LUIZ TONON on 25/08/21.
+//
+
+import UIKit
+
+class TononTela02ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
