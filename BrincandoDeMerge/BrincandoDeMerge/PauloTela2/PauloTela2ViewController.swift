4b5c69236506426a3d740cc881abd0b0cdc8c07a
Paulo Ãnes de Souza Rodrigues <pauloadesr@hotmail.com>
2021-08-25 15:39:03 -0300

    tela 2 criada

---
+++ BrincandoDeMerge/BrincandoDeMerge/PauloTela2/PauloTela2ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PauloTela2ViewController.swift
+//  BrincandoDeMerge
+//
+//  Created by Paulo Rodrigues on 25/08/21.
+//
+
+import UIKit
+
+class PauloTela2ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
